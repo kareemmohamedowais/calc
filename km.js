642f95b054ba91cd63303d9676709ea3f7099bb5
kareem <01113604940k@gmail.com>
2024-11-28 19:12:37 +0200

    =calc

---
+++ km.js
@@ -0,0 +1,20 @@
+let output = document.getElementById("calc")
+
+function display(num){
+    output.value += num;
+}
+function clear() {
+    output.value = "";
+}
+function Delete(){
+    output.value= output.value.slice(0, -1);
+}
+
+function calc() {
+    try{
+        output.value = eval(output.value);
+    }
+    catch(err){
+        output.value = "error";
+    }
+}
